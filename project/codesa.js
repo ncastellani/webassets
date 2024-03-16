b603f2c982cb7876a4363ea5e14f752881f9f8c3
ncastellani <me@ncastellani.com>
2024-03-16 17:59:45 -0300

    changed the developmentMsg from CODESA

--- project/codesa.js
+++ project/codesa.js
@@ -1,5 +1,5 @@
 
-const developmentMsg = 'CODESA Software LTDA\r\nS\u00E3o Paulo\/SP\r\n\r\n \n   ______   ____     ____     ______   _____    ___ \n  / ____/  / __ \\   / __ \\   / ____/  / ___/   /   |\n / /      / / / /  / / / /  / __/     \\__ \\   / /| |\n/ /___   / /_/ /  / /_/ /  / /___    ___/ /  / ___ |\n\\____/   \\____/  /_____/  /_____/   /____/  /_/  |_|\n                                                    \n \r\n\r\nhttps:\/\/codesa.com.br\/\r\nHost: ' + window.location.hostname;
+const developmentMsg = 'CODESA Software LTDA - S\u00E3o Paulo\/SP\r\n\r\n \n   ______   ____     ____     ______   _____    ___ \n  / ____/  / __ \\   / __ \\   / ____/  / ___/   /   |\n / /      / / / /  / / / /  / __/     \\__ \\   / /| |\n/ /___   / /_/ /  / /_/ /  / /___    ___/ /  / ___ |\n\\____/   \\____/  /_____/  /_____/   /____/  /_/  |_|\n                                                    \n \r\n\r\nhttps:\/\/codesa.com.br\/\r\nHost: ' + window.location.hostname;
 
 function getWebServiceURL() {
     return '';
