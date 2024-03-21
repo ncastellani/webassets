c7cd55b4d267e636ddd6b96065a2effec6efa504
ncastellani <me@ncastellani.com>
2024-03-21 20:40:35 -0300

    added a whole new set of util functions

--- js/general.js
+++ js/general.js
@@ -3,31 +3,7 @@ var site = {}; // customer custom preferences
 var lang = {}; // language translations and preferences
 var pageLang = document.querySelector('meta[name="language"]').content.toLowerCase(); // HTML file language
 
-// !! fetch files from the requested path
-function loadFile(path, cb) {
-    console.debug(`Utils: trying to load file [path: ${path}]`);
-
-    let req = new XMLHttpRequest();
-
-    req.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                cb(this.responseText);
-            } else {
-                //addLoadingOverlayProgressMessage(`${lang.t.tryingToLoadFile} (${path})`, false, null, null);
-                //updateLoadingOverlayDescription(lang.t.errorHandlingData, 100, 'error');
-                //showLoadingOverlay();
-                return;
-            }
-        }
-    };
-
-    req.open('GET', path, true);
-    req.send();
-
-}
-
-// fetch JSON files from the requested path
+// !! fetch JSON files from the requested path
 function loadJSONFile(path, cb) {
     loadFile(path, function(data) {
         cb(JSON.parse(data));
@@ -35,52 +11,11 @@ function loadJSONFile(path, cb) {
 
 }
 
-// update the browser theme-color meta tag with the passed color
-function updateBrowserTheme(color) {
-    let metaThemeColor = document.querySelector('meta[name=theme-color]');
-    metaThemeColor.setAttribute('content', color);
+// !! show a notification for the user
+function throwMessage(type, title, msg) {
+    console.debug(`Utils: creating a user notification [type: ${type}] [title: ${title}] [msg: ${msg}]`);
 
-    return true;
-}
-
-// check if the array contains a value
-function contains(a, obj) {
-    let i = a.length;
-    while (i--) { if (a[i] === obj) { return true; } }
-    return false;
-}
-
-// remove the .active class from all IDs that has the passed prefix
-function removeActiveFromPrefix(prefix, exceptIDs) {
-    console.debug(`Utils: removing .active from set of elements [prefix: ${prefix}] [except: ${exceptIDs}]`);
-
-    let allItems = document.querySelectorAll('*[id^="' + prefix + '"]');
-    for (i = 0; i < allItems.length; ++i) {
-        if (!contains(exceptIDs, allItems[i].id)) {
-            document.getElementById(allItems[i].id).classList.remove('active');
-        }
-    }
-
-}
-
-// set an interval to add the .active on a element if exists
-function tryToAddActiveToItem(id) {
-    console.debug(`Utils: registed a intent to add .active [id: ${id}]`);
-
-    let tryCount = 0;
-    let setItemActiveInterval = setInterval(function() {
-        if (tryCount == 10) {
-            clearInterval(setItemActiveInterval);
-            console.debug(`Utils: giving up on adding the .active to element [id: ${id}]`);
-        } else {
-            if (document.getElementById(id)) {
-                document.getElementById(id).classList.add('active');
-                clearInterval(setItemActiveInterval);
-                console.debug(`Utils: sucessfully added the .active to element [id: ${id}]`);
-            } else {
-                tryCount++;
-            }
-        }
-    }, 100);
+    console.debug(`Utils: this browser does not support promises. the user notification will be sent as an browser alert`);
+    alert(msg);
 
 }
---
+++ js/utils.js
@@ -0,0 +1,163 @@
+
+// truncate an string on the informed size length
+function truncate(source, size) {
+    return source.length > size ? source.slice(0, size - 1) + '...' : source;
+}
+
+// generate an random 12-digits code
+function uuid() {
+    return 'axxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+// get an parameter from the URL path
+function getURLParam(name) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    let url = window.location.href;
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), results = regex.exec(url);
+
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+// remove a param from the query string returing URL
+function removeURLParam(name) {
+    console.debug(`Utils: assembling an URL without a query string param [key: ${name}]`);
+
+    let url = window.location.href;
+    let urlparts = url.split('?');
+
+    if (urlparts.length >= 2) {
+
+        let prefix = encodeURIComponent(name) + '=';
+        let pars = urlparts[1].split(/[&;]/g);
+
+        for (let i = pars.length; i-- > 0;) {
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+
+    return url;
+}
+
+// check if the user has access to internet
+function userOnline() {
+    return ('onLine' in navigator ? navigator.onLine : true);
+}
+
+// get the current timestamp in milliseconds
+function getTime() {
+    return Date.now();
+}
+
+// receive a number of bytes and sorts it into an readable unit
+function bytesToReadable(total, unit) {
+    if (0 == total) return '0 Bytes';
+    let c = 1024,
+        d = unit || 2,
+        e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        f = Math.floor(Math.log(total) / Math.log(c));
+    return parseFloat((total / Math.pow(c, f)).toFixed(d)) + ' ' + e[f];
+}
+
+// copy an string to the clipboard
+function toClipboard(data) {
+    console.debug(`Utils: copying data to the clipboard [data: ${data}]`);
+
+    let el = document.createElement('textarea');
+    el.value = data;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+}
+
+// check if the array contains a value
+function contains(a, obj) {
+    let i = a.length;
+    while (i--) { if (a[i] === obj) { return true; } }
+    return false;
+}
+
+// convert a object to an HTML-placable JSON string
+function objectToJSONString(data) {
+    data = JSON.stringify(data);
+    return data.replace(/"/g, '&quot;');
+}
+
+// fetch the file from the requested path
+function loadFile(path, cb) {
+    console.debug(`Utils: trying to load file [path: ${path}]`);
+
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                cb(this.responseText);
+            } else {
+                cb(false);
+                return;
+            }
+        }
+    };
+
+    req.open('GET', path, true);
+    req.send();
+
+}
+
+// update the browser theme-color meta tag with the passed color
+function updateBrowserTheme(color) {
+    let metaThemeColor = document.querySelector('meta[name=theme-color]');
+    metaThemeColor.setAttribute('content', color);
+
+    return true;
+}
+
+// remove the .active class from all IDs that has the passed prefix
+function removeActiveFromPrefix(prefix, exceptIDs) {
+    console.debug(`Utils: removing .active from set of elements [prefix: ${prefix}] [except: ${exceptIDs}]`);
+
+    let allItems = document.querySelectorAll('*[id^="' + prefix + '"]');
+    for (i = 0; i < allItems.length; ++i) {
+        if (!contains(exceptIDs, allItems[i].id)) {
+            document.getElementById(allItems[i].id).classList.remove('active');
+        }
+    }
+
+}
+
+// set an interval to add the .active on a element if exists
+function tryToAddActiveToItem(id) {
+    console.debug(`Utils: registed a intent to add .active [id: ${id}]`);
+
+    let tryCount = 0;
+    let setItemActiveInterval = setInterval(function() {
+        if (tryCount == 10) {
+            clearInterval(setItemActiveInterval);
+            console.debug(`Utils: giving up on adding the .active to element [id: ${id}]`);
+        } else {
+            if (document.getElementById(id)) {
+                document.getElementById(id).classList.add('active');
+                clearInterval(setItemActiveInterval);
+                console.debug(`Utils: sucessfully added the .active to element [id: ${id}]`);
+            } else {
+                tryCount++;
+            }
+        }
+    }, 100);
+
+}
